f206c28c77c52f984a2ede2ae56860818321df5c
tao_ling <xcelerent@gmail.com>
2024-05-27 15:06:34 +1000

    Correct setup.sh file store path

--- tf-az-kw-nifi/dev/setup.sh
+++ tf-az-kw-nifi/dev/setup.sh
@@ -14,7 +14,7 @@ export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Download .profile from GitHub and permanently set JAVA_HOME and PATH
-wget -O /temp/.profil https://raw.githubusercontent.com/your-repo/your-project/main/.profile
+wget -O /temp/.profil https://raw.githubusercontent.com/xcelerent/file-store/main/tf-az-kw-nifi/.profile
 sudo cp /tmp/.profile /home/lingtao/.profile
 
 # Download latest nifi 1.2x zip
@@ -24,7 +24,7 @@ wget https://dlcdn.apache.org/nifi/1.26.0/nifi-1.26.0-bin.zip
 unzip nifi-1.26.0-bin.zip
 
 # Download nifi.properties from GitHub
-wget -O /temp/nifi.properties https://raw.githubusercontent.com/your-repo/your-project/main/nifi.properties
+wget -O /temp/nifi.properties https://raw.githubusercontent.com/xcelerent/file-store/main/tf-az-kw-nifi/dev/nifi.properties
 
 # Overwrite the existing nifi.properties file
 sudo cp /tmp/nifi.properties /home/lingtao/nifi-1.26.0/conf/nifi.properties
